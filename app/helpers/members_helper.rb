a7f095b7e5bd32471ac4323dcd802a80af9e8ff9
Celine Patag Miranda <cpatag@iripple.net>
2023-06-25 16:14:39 +0800

    added shape and color for in law

--- app/helpers/members_helper.rb
+++ app/helpers/members_helper.rb
@@ -7,4 +7,15 @@ module MembersHelper
     connection(member).nil? ? 'null' : connection(member).id
   end
 
+  def yod member
+    member.death.nil? ? ' ' : member.death
+  end
+
+  def shape member
+    member.in_law ? 'diamond' : 'circle'
+  end
+
+  def color member
+    member.in_law ? "#ffa000" : "#005EFF"
+  end
 end
