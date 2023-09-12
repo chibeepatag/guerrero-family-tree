c1fd29261bdfb37f60b4d599174821bbfc83a219
Celine Patag Miranda <cpatag@iripple.net>
2023-09-12 15:10:07 +0800

    added method to get descendants

--- app/models/member.rb
+++ app/models/member.rb
@@ -3,4 +3,15 @@ class Member < ApplicationRecord
   belongs_to :parent, class_name: "Member", optional: true
   belongs_to :mother, class_name: "Member", optional: true
   belongs_to :father, class_name: "Member", optional: true
+
+  def descendants
+    results = []
+    children = Member.where(mother_id: id).or(Member.where(father_id: id))
+    children.each do |child|
+      results << child
+      results << child.spouse
+      results << child.descendants
+    end
+    results.flatten.compact
+  end
 end
