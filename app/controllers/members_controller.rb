30c3013c79e66db2e367e430c9cad48bfcc4ca82
Celine Patag Miranda <cpatag@iripple.net>
2023-09-12 15:28:26 +0800

    need parent param so no line extends out of parent

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -7,7 +7,14 @@ class MembersController < ApplicationController
   end
 
   def tree
-    @members = Member.all
+    if params[:parent_id].present?
+      parent1 = Member.find params[:parent_id]
+      parent2 = parent1.spouse
+      @members = [parent1, parent2].concat(parent1.descendants).flatten
+    else
+      @members = Member.all
+    end
+
     @nodes = @members
   end
 
