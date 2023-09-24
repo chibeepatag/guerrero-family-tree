bb5222cf295ae546586edca43655a6ca727d981f
Celine Patag Miranda <cpatag@iripple.net>
2023-09-24 18:30:18 +0800

    use importmaps, cant get chart js to work

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,4 +3,15 @@ import "popper"
 import "bootstrap"
 import "FamilyTree"
 import "autoComplete"
+import Chart from 'chart.js'
 import "controllers"
+//import "../scss/app.scss";
+
+// AdminKit (required)
+import "./modules/sidebar";
+import "./modules/theme";
+import "feather";
+// Forms
+import "flatpickr";
+// Maps
+import "jsVectorMap";
--- config/importmap.rb
+++ config/importmap.rb
@@ -7,4 +7,7 @@ pin "autoComplete", to: "https://ga.jspm.io/npm:@tarekraafat/autocomplete.js@10.
 pin "FamilyTree", to: "familytree.js" 
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin "flatpickr", to: "https://ga.jspm.io/npm:flatpickr@4.6.13/dist/esm/index.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "chart.js", to: "https://ga.jspm.io/npm:chart.js@4.4.0/dist/chart.js"
+pin "@kurkle/color", to: "https://ga.jspm.io/npm:@kurkle/color@0.3.2/dist/color.esm.js"
