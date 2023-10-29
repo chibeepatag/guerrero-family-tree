524421f7abded4fc7da2721ab5b0e8ef35b80f9a
Celine Patag Miranda <cpatag@iripple.net>
2023-10-29 10:08:24 +0800

    all birth order has to be filled else tree is messed up

--- app/controllers/tree_controller.rb
+++ app/controllers/tree_controller.rb
@@ -12,9 +12,9 @@ class TreeController < ApplicationController
 
   def branch
     branch_name = params[:branch_name]
-    @members = Member.where(branch: branch_name).order(:birth_order)
+    @members = Member.where(branch: branch_name)#.order(:birth_order)
     if "Leon".eql? branch_name
-      @members = @members.or(Member.where(is_head: true)).order(:birth_order)
+      @members = @members.or(Member.where(is_head: true))#.order(:birth_order)
     end
   end
   
