5d0bf75d207174e3cc989162535ec26c7ec2438b
Celine Patag Miranda <cpatag@iripple.net>
2023-12-05 09:17:11 +0800

    refactor to reduce repetitive code

--- app/javascript/controllers/tree_controller.js
+++ app/javascript/controllers/tree_controller.js
@@ -9,6 +9,14 @@ export default class extends Controller {
   }
 
   createTemplates(){
+    function nodeTemplate(color, isMale){
+      if(isMale){
+        return `<line x1="13" x2="13" y1="0" y2="90" stroke="#${color}" stroke-width="25" opacity="0.35"/><rect x="25" y="0" height="90" width="300" stroke-width="0" fill="#F1F1F1" ></rect>;`
+      } else {
+        return `<line x1="13" x2="13" y1="0" y2="90" stroke="#${color}" stroke-width="25"/><rect x="25" y="0" height="90" width="300" stroke-width="0" fill="#F1F1F1"></rect>`;
+      }
+    }
+    
     FamilyTree.templates.red = Object.assign({}, FamilyTree.templates.tommy);
     FamilyTree.templates.red.size = [350, 90]; 
     FamilyTree.templates.red.field_0 = '<text  data-text-overflow="ellipsis" style="font-size: 15px; font-weight: bold" fill="#222831" x="150" y="37" text-anchor="middle">{val}</text>'
@@ -18,45 +26,45 @@ export default class extends Controller {
     FamilyTree.templates.red.field_4 = '<text  data-text-overflow="ellipsis" style="font-size: 15px;" fill="#222831" x="230" y="80" text-anchor="middle">YOD: {val}</text>';
               
     FamilyTree.templates.red_male = Object.assign({}, FamilyTree.templates.red);
-    FamilyTree.templates.red_male.node = '<line x1="13" x2="13" y1="0" y2="90" stroke="#FF5757" stroke-width="25" opacity="0.35"/><rect x="25" y="0" height="90" width="300" stroke-width="0" fill="#F1F1F1" ></rect>';
+    FamilyTree.templates.red_male.node = nodeTemplate('FF5757', true);
     FamilyTree.templates.red_female = Object.assign({}, FamilyTree.templates.red);
-    FamilyTree.templates.red_female.node = '<line x1="13" x2="13" y1="0" y2="90" stroke="#FF5757" stroke-width="25"/><rect x="25" y="0" height="90" width="300" stroke-width="0" fill="#F1F1F1"></rect>';
+    FamilyTree.templates.red_female.node = nodeTemplate('FF5757', false);
 
     FamilyTree.templates.orange = Object.assign({}, FamilyTree.templates.red);
     FamilyTree.templates.orange_male = Object.assign({}, FamilyTree.templates.orange);
-    FamilyTree.templates.orange_male.node = '<line x1="13" x2="13" y1="0" y2="90" stroke="#FF7729" stroke-width="25" opacity="0.35"/><rect x="25" y="0" height="90" width="300" stroke-width="0" fill="#F1F1F1" ></rect>';
+    FamilyTree.templates.orange_male.node = nodeTemplate('FF7729', true);
     FamilyTree.templates.orange_female = Object.assign({}, FamilyTree.templates.orange);
-    FamilyTree.templates.orange_female.node = '<line x1="13" x2="13" y1="0" y2="90" stroke="#FF7729" stroke-width="25"/><rect x="25" y="0" height="90" width="300" stroke-width="0" fill="#F1F1F1"></rect>';
+    FamilyTree.templates.orange_female.node = nodeTemplate('FF7729', false);
 
     FamilyTree.templates.yellow = Object.assign({}, FamilyTree.templates.red);
     FamilyTree.templates.yellow_male = Object.assign({}, FamilyTree.templates.yellow);
-    FamilyTree.templates.yellow_male.node = '<line x1="13" x2="13" y1="0" y2="90" stroke="#FFB604" stroke-width="25" opacity="0.35"/><rect x="25" y="0" height="90" width="350" stroke-width="0" fill="#F1F1F1" ></rect>';
+    FamilyTree.templates.yellow_male.node = nodeTemplate('FFB604', true);
     FamilyTree.templates.yellow_female = Object.assign({}, FamilyTree.templates.yellow);
-    FamilyTree.templates.yellow_female.node = '<line x1="13" x2="13" y1="0" y2="90" stroke="#FFB604" stroke-width="25"/><rect x="25" y="0" height="90" width="350" stroke-width="0" fill="#F1F1F1"></rect>';
+    FamilyTree.templates.yellow_female.node = nodeTemplate('FFB604', false);
 
     FamilyTree.templates.green = Object.assign({}, FamilyTree.templates.red);
     FamilyTree.templates.green_male = Object.assign({}, FamilyTree.templates.green);
-    FamilyTree.templates.green_male.node = '<line x1="13" x2="13" y1="0" y2="90" stroke="#0DC86F" stroke-width="25" opacity="0.35"/><rect x="25" y="0" height="90" width="300" stroke-width="0" fill="#F1F1F1" ></rect>';
+    FamilyTree.templates.green_male.node = nodeTemplate('0DC86F', true);
     FamilyTree.templates.green_female = Object.assign({}, FamilyTree.templates.green);
-    FamilyTree.templates.green_female.node = '<line x1="13" x2="13" y1="0" y2="90" stroke="#0DC86F" stroke-width="25"/><rect x="25" y="0" height="90" width="300" stroke-width="0" fill="#F1F1F1"></rect>';
+    FamilyTree.templates.green_female.node = nodeTemplate('0DC86F', false);
 
     FamilyTree.templates.blue = Object.assign({}, FamilyTree.templates.red);
     FamilyTree.templates.blue_male = Object.assign({}, FamilyTree.templates.blue);
-    FamilyTree.templates.blue_male.node = '<line x1="13" x2="13" y1="0" y2="90" stroke="#2772FF" stroke-width="25" opacity="0.35"/><rect x="25" y="0" height="90" width="300" stroke-width="0" fill="#F1F1F1" ></rect>';
+    FamilyTree.templates.blue_male.node = nodeTemplate('2772FF', true);
     FamilyTree.templates.blue_female = Object.assign({}, FamilyTree.templates.blue);
-    FamilyTree.templates.blue_female.node = '<line x1="13" x2="13" y1="0" y2="90" stroke="#2772FF" stroke-width="25"/><rect x="25" y="0" height="90" width="300" stroke-width="0" fill="#F1F1F1"></rect>';
+    FamilyTree.templates.blue_female.node = nodeTemplate('2772FF', false);
 
     FamilyTree.templates.lavender = Object.assign({}, FamilyTree.templates.red);
     FamilyTree.templates.lavender_male = Object.assign({}, FamilyTree.templates.lavender);
-    FamilyTree.templates.lavender_male.node = '<line x1="13" x2="13" y1="0" y2="90" stroke="#AE9CD6" stroke-width="25" opacity="0.35"/><rect x="25" y="0" height="90" width="300" stroke-width="0" fill="#F1F1F1" ></rect>';
+    FamilyTree.templates.lavender_male.node = nodeTemplate('AE9CD6', true);
     FamilyTree.templates.lavender_female = Object.assign({}, FamilyTree.templates.lavender);
-    FamilyTree.templates.lavender_female.node = '<line x1="13" x2="13" y1="0" y2="90" stroke="#AE9CD6" stroke-width="25"/><rect x="25" y="0" height="90" width="300" stroke-width="0" fill="#F1F1F1"></rect>';
+    FamilyTree.templates.lavender_female.node = nodeTemplate('AE9CD6', false);
 
     FamilyTree.templates.beige = Object.assign({}, FamilyTree.templates.red);
     FamilyTree.templates.beige_male = Object.assign({}, FamilyTree.templates.beige);
-    FamilyTree.templates.beige_male.node = '<line x1="13" x2="13" y1="0" y2="90" stroke="#964b00 " stroke-width="25" opacity="0.35"/><rect x="25" y="0" height="90" width="300" stroke-width="0" fill="#F1F1F1" ></rect>';
+    FamilyTree.templates.beige_male.node = nodeTemplate('964b00', true);
     FamilyTree.templates.beige_female = Object.assign({}, FamilyTree.templates.beige);
-    FamilyTree.templates.beige_female.node = '<line x1="13" x2="13" y1="0" y2="90" stroke="#964b00 " stroke-width="25"/><rect x="25" y="0" height="90" width="300" stroke-width="0" fill="#F1F1F1"></rect>';
+    FamilyTree.templates.beige_female.node = nodeTemplate('964b00', false);
   }
   renderTree(nodes){
     let family = new FamilyTree(document.getElementById("tree"), {
