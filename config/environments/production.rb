18f3d92fe9e9260faf7d69446e693e0f82feaca5
Celine Patag Miranda <cpatag@iripple.net>
2024-01-05 11:24:40 +0800

    added production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.family_name = "Guerrero"
+  config.admin_code = "devpro116"
 end
